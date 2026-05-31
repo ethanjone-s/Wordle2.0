9c5c71442b5c9e249302b0aaef4d959e1cbe2ccd
Ethan <ighxsty@gmail.com>
2026-05-31 01:27:32 -0500

    title name change

--- base.py
+++ base.py
@@ -211,7 +211,7 @@ keyboardLayout=[
 class wordle:
     def __init__(self,root,words,scale=1.0):
         self.root=root
-        self.root.title('Wordle 2.0')
+        self.root.title('Wordlite')
         self.root.configure(bg='#ffffff')
         self.root.resizable(False,False)
 
